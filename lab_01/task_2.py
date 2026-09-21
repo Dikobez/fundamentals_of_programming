2e6ac957e05352a28f2774c95c3695ab51162c9e
Dikobez <151544146+Dikobez@users.noreply.github.com>
2026-09-21 16:57:21 +0300

    Create task_2.py

---
+++ lab_01/task_2.py
@@ -0,0 +1,20 @@
+a = 1000
+b = a
+c = int("1000")
+
+print("Типы:", type(a), type(b), type(c))
+print("Идентификаторы:", id(a), id(b), id(c))
+print("a == b:", a == b)
+print("a is b:", a is b)
+print("a == c:", a == c)
+print("a is c:", a is c)
+
+c = None
+if с == None:
+  print('с == None')
+
+first = "python"
+second = "py" + "thon"
+
+print(first == second)
+print(first is second)
